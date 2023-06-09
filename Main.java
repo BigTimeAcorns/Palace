f1bff275c096ce10f89db99ebbc8328a8074df58
BigTimeAcorns <cadejdannels@gmail.com>
2023-06-09 14:30:31 -0700

    ffff

--- Main.java
+++ Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Palace p = new Palace("New York");
-        p.setLock("APCSA");
-        p.setLock("password")
-        p.setColor("purple");
-        p.setLock("password");
-        p.setColor("purple");
-        p.setColor("red");
-        p.getColor();
-        p.addRoom();
-        p.getRooms();
-        p.getLocation();
+        System.out.println(p.setLock("APCSA"));
+        System.out.println(p.setLock("password"));
+        System.out.println(p.setColor("purple"));
+        System.out.println(p.setLock("password"));
+        System.out.println(p.setColor("purple"));
+        System.out.println(p.setColor("red"));
+        System.out.println(p.getColor());
+        System.out.println(p.addRoom());
+        System.out.println(p.addRoom());
+        System.out.println(p.getRooms());
+        System.out.println(p.getLocation());
     }
 
 }
