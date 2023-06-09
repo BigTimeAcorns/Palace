d6eb8c632bbfa3db62bb40cf7f0a08344c0aa924
BigTimeAcorns <cadejdannels@gmail.com>
2023-06-09 14:26:54 -0700

    eeee

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Palace p = new Palace("New York");
+        p.setLock("APCSA");
+        p.setLock("password")
+        p.setColor("purple");
+        p.setLock("password");
+        p.setColor("purple");
+        p.setColor("red");
+        p.getColor();
+        p.addRoom();
+        p.getRooms();
+        p.getLocation();
+    }
+
+}
--- Palace.java
+++ Palace.java
@@ -1,33 +1,72 @@
 public class Palace {
+
+    // These are all the local variables for the class
     private boolean locked;
     private int numberOfRooms;
-    public String location;
+    private String location;
+    private String color;
 
+    // The constructor for the object and all its attributes
     public Palace(String l) {
         location = l;
         locked = false;
         numberOfRooms = 0;
+        color = "white";
     }
 
-    public void paint(String color) {
+    // Takes a color and sees if its a valid color
+    public String setColor(String c) {
         if (!locked) {
-            System.out.println("Painting the palace in " + color + " color.");
-        } else {
-            System.out.println("Cannot paint the palace. It is locked.");
+            String[] colors = { "white", "black", "red", "green", "yellow", "brown", "beige" };
+            for (int i = 0; i < colors.length; i++) {
+                if (c == colors[i]) {
+                    color = colors[i];
+                    return c + "is a valid color and is now the color of your palace";
+                }
+            }
+            return "That is not a valid color";
         }
+        return "The palace is locked";
     }
 
-    public void lock() {
-        locked = true;
-        System.out.println("The palace at " + location + " is locked.");
+    // returns the color of the palace
+    public String getColor() {
+        return color;
     }
 
-    public void buildRoom() {
-        if (!locked) {
-            numberOfRooms++;
-            System.out.println("A new room is added to the palace. Total rooms: " + numberOfRooms);
-        } else {
-            System.out.println("Cannot build a room. The palace is locked.");
-        }
+    // returns whether or not the palace is locked
+    public boolean getLocked() {
+        return locked;
     }
+
+    // returns the number of rooms in the palace
+    public int getRooms() {
+        return numberOfRooms;
+    }
+
+    // returns the location
+    public String getLocation() {
+        return location;
+    }
+
+    // sets the locked variable to either true or false passed on the password and
+    // the current value
+    public String setLock(String p) {
+        if (p == "password") {
+            if (locked) {
+                locked = false;
+                return "locked = false";
+            }
+            locked = true;
+            return "locked = true";
+        }
+        return "wrong password";
+    }
+
+    // adds 1 to the room variable
+    public String addRoom() {
+        numberOfRooms++;
+        return "added a room to palace";
+    }
+
 }
